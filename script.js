4d247aea866f61d2ddaadfe1d6e3933831641ac9
Rahul Prasad Shaw <61372250+rjrahul08@users.noreply.github.com>
2023-07-19 13:07:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 
+let yourArray = ["Hello", 42, true, "World", false];
+let complexArray = [
+  [
+    {
+      one: 1,
+      two: 2
+    },
+    {
+      three: 3,
+      four: 4
+    }
+  ],
+  [
+    {
+      a: "a",
+      b: "b"
+    },
+    {
+      c: "c",
+      d: "d"
+    }
+  ]
+];
+
+console.log(complexArray.length); // logs 5
